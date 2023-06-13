9ab138b2d3621f89dd7c6cef71af5a63f2877277
Hyun <87049207+vaynel@users.noreply.github.com>
2023-06-13 11:53:16 +0900

    studyRoom top drawing

--- frontend/src/portal/studyRoom/PersonalStodyRoom.vue
+++ frontend/src/portal/studyRoom/PersonalStodyRoom.vue
@@ -4,11 +4,15 @@
     <img class="room_bottom2" src="../../assets/images/room_bottom2.png">
     <img class="bookshelf" src="../../assets/images/bookshelf.png" >
     <img class="kirby" src="../../assets/images/kirby_on_table.png">
-     <img class="table" src="../../assets/images/table_cloud.png">
-     <div class="subject"> 
-        스프링
-        </div> 
-        <div class="timer">00:00:00</div>
+    <img class="desk" src="../../assets/images/desk.png">
+    <img class="radio" src="../../assets/images/radio.png">
+    <img class="radioDesk" src="../../assets/images/radio_desk.png">
+     <img class="portal" src="../../assets/images/portal_star.png">
+    
+     <div class="subject">  스프링 - 
+    <span class="timer"> 00:00:00</span></div>
+
+    <div class="status"> 공부중 </div>
 
 
 </div>
